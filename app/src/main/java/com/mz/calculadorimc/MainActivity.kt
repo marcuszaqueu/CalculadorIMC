f6782bf08f950aab293b0c781e055505f1257334
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-03-08 14:37:53 -0300

    Initial commit
    Declaração de variaveis, calculo de imc, criação de menu e função para limpeza de campos,
    
    Criação do icone de reset
    
    Criação do Menu Principal

--- app/src/main/java/com/mz/calculadorimc/MainActivity.kt
+++ app/src/main/java/com/mz/calculadorimc/MainActivity.kt
@@ -2,10 +2,79 @@ package com.mz.calculadorimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import com.mz.calculadorimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bt_calcular = binding.btnCalulo
+        val mensagem = binding.mensagem
+
+        bt_calcular.setOnClickListener {
+            val editPeso = binding.editPeso.text.toString()
+            val editAltura = binding.editAltura.text.toString()
+
+            if(editPeso.isEmpty()){
+                mensagem.setText("Informe o seu Peso!")
+            }else if (editAltura.isEmpty()){
+                mensagem.setText("Informe sua Altura!")
+            }else{
+                CalculoIMC()
+            }
+        }
+
+    }
+    private fun CalculoIMC(){
+        val pesoId = binding.editPeso
+        val alturaId = binding.editAltura
+        val peso = Integer.parseInt(pesoId.text.toString())
+        val altura = java.lang.Float.parseFloat(alturaId.text.toString())
+        val imc = peso / (altura * altura)
+        val resultado = binding.mensagem
+
+        val Mensagem = when{
+            imc <= 18.5 -> "Peso Baixo"
+            imc <= 24.9 -> "Peso Normal"
+            imc <= 29.9 -> "Sobrepeso"
+            imc <= 34.9 -> "Obesidade (Grau 1)"
+            imc <= 39.9 -> "Obesidade (Grau 2)"
+            else -> "Obesidade Mórbida (Grau 3)"
+
+        }
+        imc.toString()
+        resultado.setText("IMC: $imc \n $Mensagem")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.menu_principal,menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.reset ->{
+                val limparPeso = binding.editPeso
+                val limparAltura = binding.editAltura
+                val limparMensagem = binding.mensagem
+
+                limparPeso.setText("")
+                limparAltura.setText("")
+                limparMensagem.setText("")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
